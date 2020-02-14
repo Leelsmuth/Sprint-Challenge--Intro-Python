7ba5a9fdd6d9b9a11c34975fd48a848fc0f0f27e
Leelsmuth <femianjorin@rocketmail.com>
2020-02-14 11:42:39 +0100

    MVP completed, tests passing

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -4,11 +4,11 @@ import csv
 class City:
   def __init__(self, name, lat, lon):
     self.name = name
-    self.lat = lat
-    self.lon = lon
+    self.lat = float(lat)
+    self.lon = float(lon)
     
   def __str__(self):
-    return f'City: {self.name}, Latitude: {self.lat}, Longitude: {self.lon} ' 
+    return f'{self.name}, {self.lat}, {self.lon} ' 
 
 # We have a collection of US cities with population over 750,000 stored in the
 # file "cities.csv". (CSV stands for "comma-separated values".)
@@ -29,8 +29,8 @@ def cityreader(cities=[]):
   # For each city record, create a new City instance and add it to the 
   # `cities` list
   with open('src/cityreader/cities.csv') as csvfile:
-      next(csvfile)
-      reader = csv.reader(csvfile)
+      reader = csv.reader(csvfile, delimiter=',')
+      next(reader)
       for row in reader:
         cities.append(City(row[0], row[3], row[4]))
   return cities
